eb4520b4f95747692748a32317c78fc0c035c8e1
Mrs Raindream <46651202+MrsRaindream@users.noreply.github.com>
2020-10-11 22:57:57 +0700

    Update MergeLogicAsArrayTest.java

--- src/test/java/com/huawei/mergeArrays/MergeLogicAsArrayTest.java
+++ src/test/java/com/huawei/mergeArrays/MergeLogicAsArrayTest.java
@@ -1,7 +1,6 @@
 package test.java.com.huawei.mergeArrays;
 
 import main.java.com.huawei.mergeArrays.MergeLogicAsArray;
-import main.java.com.huawei.util.dataProduce.ArrayProvider;
 
 import java.util.Arrays;
 
@@ -13,30 +12,25 @@ import java.util.Arrays;
 public class MergeLogicAsArrayTest {
 
     /**
-     * Size of array
-     */
-    public static final int ARRAY_SIZE = 10;
-
-    /**
      * Test of the MergeLogicAsArray
      *
      * @param args
      */
     public static void main(String[] args) {
         //size of array x
-        int m = ARRAY_SIZE;
+        int m = 3;
         //size of array y
-        int n = m / 2;
+        int n = 3;
 
-        int[] arrayX = ArrayProvider.provideSortedArray(m);
-        int[] arrayY = ArrayProvider.provideSortedArray(n);
+        int[] arrayX = new int[] {1,5,7,0,0,0};
+        int[] arrayY = new int[] {1,8,9};
 
         System.out.println("First array: ");
         System.out.println(Arrays.toString(arrayX));
         System.out.println("Second array: ");
         System.out.println(Arrays.toString(arrayY));
 
-        int[] resultArray = MergeLogicAsArray.mergeArray(arrayX, arrayY);
+        int[] resultArray = MergeLogicAsArray.mergeArray(arrayX, m, arrayY, n);
         System.out.println("Result array: ");
         System.out.println(Arrays.toString(resultArray));
     }
